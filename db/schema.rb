bad1bce720e091219ed3cce5fef0bdfc9155b3cb
moneldon <moneldon@gmail.com>
2012-03-01 01:31:43 +0530

    created user databse

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120229194114) do
+ActiveRecord::Schema.define(:version => 20120229195818) do
 
   create_table "monels", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
